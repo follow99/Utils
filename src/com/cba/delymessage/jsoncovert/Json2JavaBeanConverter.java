29d5991eab991d0a1a4a6dc3b132785b62b40ea2
follow99 <follow99@hotmail.com>
2018-07-28 23:38:46 +0100

    Json2JavaPOJO 类文件自动 生成器

---
+++ src/com/cba/delymessage/jsoncovert/Json2JavaBeanConverter.java
@@ -0,0 +1,30 @@
+package com.bcg.raesite.jsoncovert;
+
+import java.io.IOException;
+
+/**
+ * @author Administrator
+ * date 26/07/2018.
+ */
+public interface Json2JavaBeanConverter {
+
+    /***
+     *
+     * 使用本地 Json 文件生成 javaBean
+     * @param filePath string 本地文件路径
+     * @throws IOException IOExceptions 抛出所有IO异常
+     *
+     */
+    void fromFile(String filePath) throws IOException;
+
+    /***
+     *
+     * 从网络连接直接获得json 生成本地javaBean
+     * @param url string  URL 地址
+     * @param filename string 从url 获取的json 无法获得生成的类名 需要指定
+     * @throws IOException IOExceptions
+     *
+     */
+    void fromUrl(String url, String filename) throws IOException;
+
+}
---
+++ src/com/cba/delymessage/jsoncovert/LombokHeadingTemplate.java
@@ -0,0 +1,10 @@
+package com.bcg.raesite.jsoncovert;
+
+/**
+ * 直接在接口类中写了默认实现
+ * 该类的存在的目的只是为了可以new出来 使用接口中的某人头文件模板
+ * @author Administrator
+ * date 28/07/2018.
+ */
+public class LombokHeadingTemplate implements JavaBeanStringTemplate {
+}
