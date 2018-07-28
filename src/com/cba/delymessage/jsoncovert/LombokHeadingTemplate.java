63a5c6ae925e4a6f7c356abce33d910f86ccbe17
follow99 <follow99@hotmail.com>
2018-07-29 00:00:33 +0100

    Update LombokHeadingTemplate.java

--- src/com/cba/delymessage/jsoncovert/LombokHeadingTemplate.java
+++ src/com/cba/delymessage/jsoncovert/LombokHeadingTemplate.java
@@ -2,7 +2,7 @@ package com.bcg.raesite.jsoncovert;
 
 /**
  * 直接在接口类中写了默认实现
- * 该类的存在的目的只是为了可以new出来 使用接口中的某人头文件模板
+ * 该类的存在的目的只是为了可以new出来 使用接口中的Lombok头文件模板
  * @author Administrator
  * date 28/07/2018.
  */
