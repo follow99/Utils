c0075c5e22482210d82035e2337bdf18228c370b
follow99 <follow99@hotmail.com>
2018-07-29 18:10:38 +0100

    re-commit

---
+++ src/com/bcg/json2javabean/InputTypes.java
@@ -0,0 +1,13 @@
+package com.bcg.json2javabean;
+
+/**
+ * @author Administrator
+ * date 27/07/2018.
+ */
+public enum InputTypes {
+
+    //以此标识输入的filePath 并将 该文件转换为string使用作为是否需要转换的标识符
+    ISFILE,
+    //标识为string文件直接使用为 不需要处理逻辑
+    ISTEXT
+}
